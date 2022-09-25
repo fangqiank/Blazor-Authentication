157edd1e58941b53a7db2aa6870d43577bafd15f
fangqiank <fangqiank@yahoo.com>
2022-09-25 08:09:29 +0800

    Add project files.

---
+++ BlazorAuthentication/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,64 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorAuthentication.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, HttpClient http)
+        {
+            _localStorage = localStorage;
+            _http = http;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            var identity = new ClaimsIdentity();
+            _http.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                    "Bearer",
+                    token.Replace("\"", "")
+                    );
+            }
+
+            var state = new AuthenticationState(new ClaimsPrincipal(identity));
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string token)
+        {
+            var payload = token.Split('.')[1];
+
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            return keyValuePairs.Select(x => new Claim(x.Key, x.Value.ToString()));
+        }
+
+        private byte[] ParseBase64WithoutPadding(string payload)
+        {
+            switch (payload.Length % 4)
+            {
+                case 2: payload += "=="; break;
+                case 3: payload += "="; break;
+            }
+
+            return Convert.FromBase64String(payload);
+        }
+    }
+}
